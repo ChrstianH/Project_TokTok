d74dae181d6f5bc88960ce4ea6343e369eb13593
ChrstianH <henschel_christian@web.de>
2024-11-20 16:52:48 +0100

    NewPostPage started

--- src/pages/NewPostPage.tsx
+++ src/pages/NewPostPage.tsx
@@ -1,3 +1,51 @@
+import { useRef } from "react";
+import { useUserContext } from "../context/userContext";
+import { getStorageURL, supabase } from "../lib/supabase";
+import { useQuery } from "@tanstack/react-query";
+
+interface Post {
+  img_url: string | null;
+  text: string;
+  user_id: string;
+}
+
 export default function NewPostPage() {
-  return <div></div>;
+  const { user } = useUserContext();
+
+  const fileRef = useRef<HTMLInputElement>(null);
+
+  let imagePath: string | null = null;
+
+  const file = fileRef.current?.files?.[0] || null;
+  const handleUpload = async (e: React.FormEvent) => {
+    if (!user) {
+      return;
+    }
+
+    e.preventDefault();
+    if (file) {
+      const uploadResult = await supabase.storage
+        .from("post_img")
+        .upload(`${user.id}/${crypto.randomUUID()}`, file, { upsert: true });
+      imagePath = uploadResult.data?.fullPath || null;
+      console.log(imagePath?.toString());
+    }
+
+    await supabase.from("posts").insert({
+      img_url: imagePath,
+      text: "",
+      user_id: user.id,
+    });
+  };
+
+  return (
+    <div>
+      <h1>New Post</h1>
+      <img src="" alt="Upload" className="uploadArea" />
+      <form onSubmit={handleUpload}>
+        <input type="file" name="" id="" ref={fileRef} />
+        <button>Upload</button>
+      </form>
+    </div>
+  );
 }
