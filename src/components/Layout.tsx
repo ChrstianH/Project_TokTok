81a13f1c61e573e0f912f991c9405f94d75cf9fb
Nana Leven <nana.leven@mail.de>
2024-11-20 09:50:36 +0100

    database screenshot

---
+++ src/components/Layout.tsx
@@ -0,0 +1,10 @@
+interface LayoutProps {
+  children: React.ReactNode;
+}
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+  return <div className="container">{children}</div>;
+};
+
+export default Layout;
+1;
